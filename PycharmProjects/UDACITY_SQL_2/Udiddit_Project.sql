c0593949f97243b9d8ca94e9707fb2766edcdbe2
Augustas-S <auguststeik@proton.me>
2023-11-12 22:20:39 +0000

    Completed final submission.

--- PycharmProjects/UDACITY_SQL_2/Udiddit_Project.sql
+++ PycharmProjects/UDACITY_SQL_2/Udiddit_Project.sql
@@ -1,4 +1,5 @@
 --DDL
+
 CREATE TABLE users (
     "id" BIGSERIAL  PRIMARY KEY,
     "username" VARCHAR(25) UNIQUE NOT NULL,
@@ -6,16 +7,14 @@ CREATE TABLE users (
     "last_logon" TIMESTAMP WITH TIME ZONE
 );
 
-
 CREATE TABLE topics (
     "id" BIGSERIAL PRIMARY KEY,
     "user_id" BIGINT,
     "name" VARCHAR(30) UNIQUE NOT NULL,
     "created_on" TIMESTAMP WITH TIME ZONE,
-    "description" TEXT DEFAULT NULL,
-    FOREIGN KEY (user_id) REFERENCES users--???
+    "description" VARCHAR(500) DEFAULT NULL,
+    FOREIGN KEY (user_id) REFERENCES users
 );
-
 
 CREATE TABLE posts (
     "id" BIGSERIAL  PRIMARY KEY,
@@ -23,7 +22,7 @@ CREATE TABLE posts (
     "user_id" BIGINT,
     "topic_id" BIGINT,
     "title" VARCHAR(100) NOT NULL,
-    "text_content"  TEXT DEFAULT NULL,
+    "text_content" TEXT DEFAULT NULL,
     "url" TEXT DEFAULT NULL,
     CHECK ((text_content IS NULL AND url IS NOT NULL) OR (text_content IS NOT NULL AND url IS NULL)),
     FOREIGN KEY (user_id) REFERENCES users ON DELETE SET NULL,
@@ -46,13 +45,24 @@ CREATE TABLE user_votes (
     "id" BIGSERIAL PRIMARY KEY,
     "voted_on" TIMESTAMP WITH TIME ZONE,
     "user_id" BIGINT,
-    "username" VARCHAR(25),
     "post_id" BIGINT,
     "vote" SMALLINT CHECK (vote = 1 OR vote = -1),
-    UNIQUE(user_id, post_id),
+    UNIQUE (user_id, post_id),
     FOREIGN KEY (user_id) REFERENCES users ON DELETE SET NULL,
     FOREIGN KEY (post_id) REFERENCES posts ON DELETE CASCADE
 );
+
+CREATE INDEX posts_users ON posts(user_id);
+
+CREATE INDEX posts_topics ON posts(topic_id);
+
+CREATE INDEX posts_links ON posts(url);
+
+CREATE INDEX comments_parents ON comments(parent_id);
+
+CREATE INDEX comments_users ON comments(user_id);
+
+
 
 --DML
 
@@ -67,28 +77,28 @@ SELECT DISTINCT username FROM (
 ) bp1
 UNION
 SELECT DISTINCT username FROM (
-    SELECT regexp_split_to_table(downvotes, ',') AS username
+    SELECT regexp_split_to_table(downvotes, ',') AS username -- Collecting all of the unique usernames present in the database
     FROM bad_posts
 ) bp2
 ;
 
 INSERT INTO topics (name) SELECT DISTINCT topic FROM bad_posts;
 
+
 INSERT INTO posts (id, topic_id, user_id, title, text_content, url)
-SELECT bp.id,
+SELECT bp.id, -- Keeping the existing post IDs
     topics.id,
     users.id,
-    bp.title,
+    SUBSTRING(bp.title, 1, 100) AS  title, --  Trimming the titles down as some are over  100 chars
     bp.text_content,
     bp.url
 FROM bad_posts bp
-JOIN users ON users.username = bad_posts.username
-JOIN topics ON topics.name = bad_posts.topic;
+JOIN users ON users.username = bp.username
+JOIN topics ON topics.name = bp.topic;
 
--- NEED CHECKING /\  \/
 
 INSERT INTO comments (id, user_id, post_id, content)
-SELECT bc.id,
+SELECT bc.id, -- Keeping the existing comment IDs
     users.id,
     bc.post_id,
     bc.text_content
@@ -96,80 +106,7 @@ FROM bad_comments bc
 JOIN users ON users.username = bc.username;
 
 
--- navigating existing schema:
-SELECT COUNT(title) FROM bad_posts; = 50000
-SELECT DISTINCT COUNT(title) FROM bad_posts; = 50000
-
--- All titles unique.
-
---UPDATE bad_posts (title)
---INSERT INTO posts (title, text_content, url) SELECT title, text_content, url FROM bad_posts;
-
-
---CREATE TABLE user_downvotes_temp (
---    "user_idd" BIGINT,
---    "user_name" VARCHAR(100),
---    "post_id" BIGINT
---    );
---
---CREATE TABLE  user_upvotes_temp (
---    "user_idd" BIGINT,
---    "user_name" VARCHAR(100),
---    "post_id" BIGINT
---    );
-
----------------------------------------------------------------------------------------
--- Splitting up the votes
-
-
---INSERT INTO user_downvotes_temp (user_name, post_id)
---SELECT LEFT(downvotes, STRPOS(downvotes,',') -1), id FROM bad_posts
---WHERE downvotes IS NOT NULL;
---
---INSERT INTO user_downvotes_temp (user_name, post_id)
---SELECT RIGHT ( downvotes, STRPOS ( downvotes, ',') +1), id FROM bad_posts
---WHERE downvotes IS NOT NULL;
---
---INSERT INTO user_upvotes_temp (user_name, post_id)
---SELECT LEFT ( upvotes, STRPOS ( upvotes, ',') -1), id FROM bad_posts
---WHERE upvotes IS NOT NULL;
---
---INSERT INTO user_upvotes_temp (user_name, post_id)
---SELECT RIGHT ( upvotes, STRPOS ( upvotes, ',') +1), id FROM bad_posts
---WHERE upvotes IS NOT NULL;
---
---UPDATE user_downvotes_temp SET user_idd =
-----    (
-----        SELECT u.id
-----        FROM users u
-----        JOIN user_downvotes_temp udt
-----        ON u.username = udt.user_name
-------        LIMIT 1
-----    )
---WHERE u.username = udt.user_name
---;
---
---UPDATE user_downvotes_temp udt
---JOIN users u ON u.username = udt.
---
---INSERT INTO votes (user_id, post_id)
---SELECT user_idd, post_id
---FROM user_downvotes_temp
---CASE WHEN user_idd IS NOT NULL THEN vote = -1;
---
---INSERT INTO votes (user_id, post_id)
---SELECT user_idd, post_id
---FROM user_upvotes_temp
---CASE WHEN user_idd IS NOT NULL THEN vote = 1;
---
-
---INSERT INTO votes (user_id, user_id, post_id)
---SELECT (LEFT ( user_, STRPOS ( upvotes, ',') -1),
---    (RIGHT ( upvotes, STRPOS ( upvotes, ',') +1),
-
--------------------------------------------------------------------------------------------
---v2
-CREATE OR REPLACE VIEW udt
+CREATE OR REPLACE VIEW udt --User Down-votes Temp
 AS (
     SELECT bad_posts.id AS post_id,
     regexp_split_to_table(bad_posts.downvotes, ',') AS username
@@ -177,7 +114,7 @@ AS (
     FROM bad_posts
 );
 
-CREATE OR REPLACE VIEW uut
+CREATE OR REPLACE VIEW uut -- User Up-votes Temp
 AS (
     SELECT bad_posts.id AS post_id,
     regexp_split_to_table(bad_posts.upvotes, ',') AS username
@@ -186,23 +123,33 @@ AS (
 );
 
 
-
-INSERT INTO user_votes (username, post_id, vote)
-SELECT udt.username,
+INSERT INTO user_votes (user_id, post_id, vote)
+SELECT users.id,
     udt.post_id,
     -1
-FROM udt;
+FROM udt
+JOIN users ON  users.username = udt.username;
 
-INSERT INTO user_votes (username, post_id, vote)
-SELECT uut.username,
+INSERT INTO user_votes (user_id, post_id, vote)
+SELECT users.id,
     uut.post_id,
     1
-FROM uut;
+FROM uut
+JOIN users ON users.username = uut.username;
+
+DROP TABLE bad_posts, bad_comments CASCADE; -- Drops the two tables and their related VIEWs
 
 
-UPDATE user_votes SET user_id = (
-    SELECT users.id
-    FROM users
-WHERE users.username = user_votes.username);
+-- Testing the schema works as expected
+SELECT u.username,
+    uv.vote,
+    t.name AS topic,
+    p.title, p.text_content, p.url
+FROM  user_votes uv
+JOIN users u ON u.id = uv.user_id
+JOIN posts p ON p.id = uv.post_id
+JOIN topics t ON t.id = p.topic_id;
+
+
 
 
